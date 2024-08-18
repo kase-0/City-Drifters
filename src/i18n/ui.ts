3c7f6fa6285b743e88cd37b0bba3c4ad0539bc27
kase-0 <fernando20130072@gmail.com>
2024-08-17 21:53:12 -0600

    Agradecimientos y leadearboard agregados

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -25,8 +25,8 @@ export const ui = {
 	es: {
 		'nav.inicio': 'Inicio',
 		'nav.vota': 'Vota',
-		'nav.info': 'Información',
-		'nav.archivo': 'Archivo',
+		'nav.info': 'Agradecimientos',
+		'nav.archivo': 'Marcador',
 		'nav.legal': 'Aviso Legal',
 		'nav.privacidad': 'Privacidad',
 		'nav.cookies': 'Cookies',
@@ -34,8 +34,8 @@ export const ui = {
 	en: {
 		'nav.inicio': 'Home',
 		'nav.vota': 'Vote',
-		'nav.info': 'Information',
-		'nav.archivo': 'Archive',
+		'nav.info': 'Acknowledgments',
+		'nav.archivo': 'leaderboard',
 		'nav.legal': 'Legal Notice',
 		'nav.privacidad': 'Privacy',
 		'nav.cookies': 'Cookies',
